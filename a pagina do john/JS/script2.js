1771389f7a36f0482398798b86b6fd191f509cad
PNG <iampng209@gmail.com>
2025-11-25 21:23:01 -0300

    refactor

--- a pagina do john/JS/script2.js
+++
@@ -1,6 +0,0 @@
-function adicionarCarrinho(nome, preco) {
-    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-    carrinho.push({ nome, preco });
-    localStorage.setItem('carrinho', JSON.stringify(carrinho));
-    alert(nome + " adicionado ao carrinho!");
-}
